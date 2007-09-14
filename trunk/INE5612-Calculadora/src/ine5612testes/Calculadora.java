a3c1fdf5d2fc925a3a0031e3842665b5367d3522
brodock <brodock@a8306196-0d3a-0410-8417-2d91e4bb4074>
2007-09-14 00:40:12 +0000

    Import Inicial

---
+++ trunk/INE5612-Calculadora/src/ine5612testes/Calculadora.java
@@ -0,0 +1,36 @@
+
+package ine5612testes;
+
+/**
+ * Classe Controle da Calculadora
+ * @author Gabriel, Ramon
+ */
+public class Calculadora {
+
+    private double comando_numerico;
+    private Operacoes operacao;
+    private String texto_display;
+
+    public Calculadora() {
+    }
+
+    public void comando_numerico(int i) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public double getComando_numerico() {
+        return comando_numerico;
+    }
+
+    public void setComando_numerico(double comando_numerico) {
+        this.comando_numerico = comando_numerico;
+    }
+    
+    public void appendComando_numerico(int comando_numerico) {
+        this.comando_numerico = this.comando_numerico+comando_numerico;
+    }
+    
+    public String getTextoDisplay() {
+        return this.texto_display;
+    }
+}
